474d8912aa636fff726a423b011153d92aeff14a
julesodea <jules.odea@gmail.com>
2020-06-05 19:58:47 +1200

    new scss

--- components/nav/nav.jsx
+++ components/nav/nav.jsx
@@ -1,9 +1,29 @@
+import { useState, useRef, useEffect } from "react";
 import Link from "next/link";
 
 export default function Nav() {
+  const [toggle, setToggle] = useState(false);
+
+  const menuBarTop = useRef(null);
+  const menuBarBottom = useRef(null);
+
+  function handleClick() {
+    setToggle(!toggle);
+  }
+
   return (
-    <nav>
-      <ul>
+    <nav className={`nav`}>
+      <div className={`menubar`} onClick={handleClick}>
+        <div
+          ref={menuBarTop}
+          className={toggle ? `menubar-top` : `menubar-top toggled `}
+        />
+        <div
+          ref={menuBarBottom}
+          className={toggle ? `menubar-bottom` : `menubar-bottom toggled `}
+        />
+      </div>
+      <ul className={`link-container`}>
         <Link href="/">
           <a>One</a>
         </Link>
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
-import '../styles/global.css'
+import "../styles/global.css";
+import "../components/nav/nav.scss";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
