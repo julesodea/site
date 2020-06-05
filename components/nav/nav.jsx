233f879a55717ce55f1dc21a84fa080d6925a62f
julesodea <jules.odea@gmail.com>
2020-06-05 20:51:34 +1200

    added menu

--- components/nav/nav.jsx
+++ components/nav/nav.jsx
@@ -1,14 +1,22 @@
-import { useState, useRef, useEffect } from "react";
-import Link from "next/link";
+import { useState, useRef, useEffect } from 'react';
+import Link from 'next/link';
 
 export default function Nav() {
-  const [toggle, setToggle] = useState(false);
+  const [toggled, setToggle] = useState(false);
 
   const menuBarTop = useRef(null);
   const menuBarBottom = useRef(null);
 
   function handleClick() {
-    setToggle(!toggle);
+    setToggle(!toggled);
+    if (!toggled) {
+      document.body.classList.add('overflow');
+    } else {
+      document.body.classList.remove('overflow');
+    }
+  }
+  function removeToggle() {
+    setToggle(false);
   }
 
   return (
@@ -16,24 +24,38 @@ export default function Nav() {
       <div className={`menubar`} onClick={handleClick}>
         <div
           ref={menuBarTop}
-          className={toggle ? `menubar-top` : `menubar-top toggled `}
+          className={toggled ? `menubar-top toggled` : `menubar-top`}
         />
         <div
           ref={menuBarBottom}
-          className={toggle ? `menubar-bottom` : `menubar-bottom toggled `}
+          className={toggled ? `menubar-bottom toggled` : `menubar-bottom`}
         />
       </div>
       <ul className={`link-container`}>
-        <Link href="/">
+        <Link href='/'>
           <a>One</a>
         </Link>
-        <Link href="/">
+        <Link href='/page'>
           <a>One</a>
         </Link>
-        <Link href="/">
+        <Link href='/'>
           <a>One</a>
         </Link>
       </ul>
+
+      <div className={toggled ? 'menu toggled' : 'menu'}>
+        <ul className={toggled ? 'menu-links toggled' : 'menu-links'}>
+          <Link href='/'>
+            <a onClick={removeToggle}>HOME</a>
+          </Link>
+          <Link href='/page'>
+            <a onClick={removeToggle}>PAGE ONE</a>
+          </Link>
+          <Link href='/'>
+            <a onClick={removeToggle}>ANOTHER PAGE</a>
+          </Link>
+        </ul>
+      </div>
     </nav>
   );
 }
