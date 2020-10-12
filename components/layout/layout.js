f89a75fa4f35b556b86c3cf09b6301843ebfda13
julesodea <jules.odea@gmail.com>
2020-10-13 09:12:26 +1300

    update head for seo

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -16,12 +16,15 @@ export default function Layout({ children, home }) {
           name='description'
           content="Jules O'Dea Portfolio Web / Front End Development"
         />
+        <meta name="author" content="Jules O'Dea"/>
+        <meta name="robots" content="index, follow"/>
         <meta
           property='og:image'
           content={`https://og-image.now.sh/${encodeURI(
             siteTitle
           )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.zeit.co%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
         />
+        <meta name="keywords" content="jules o'dea odea portfolio web development front end development react"></meta>
         <meta name='og:title' content={siteTitle} />
         <meta name='twitter:card' content='summary_large_image' />
       </Head>
